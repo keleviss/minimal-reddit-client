5ea59ffce11fd52bb1958e8aaaf70b91054aff6f
Viss <kelesidisvissarion@gmail.com>
2025-03-28 01:07:31 +0200

    Moved SearchBar folder to the features folder and added the searchTermSlice.js file

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,17 +4,15 @@ import DefaultView from "../components/DefaultView/DefaultView";
 
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <div className="container">
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<DefaultView />}>
-
-            </Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <div className="container">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<DefaultView />}>
+            
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
