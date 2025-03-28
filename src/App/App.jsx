7ee5146001a5c256fe150cb98744c582ebe3fb2d
Viss <kelesidisvissarion@gmail.com>
2025-03-28 04:23:22 +0200

    Refactored App.jsx and exported page components

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import NavBar from "../components/NavBar/NavBar";
+import Home from "../pages/Home";
+import SearchResults from "../pages/SearchResults";
+import PostDetails from "../pages/PostDetails";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="container">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<DefaultView />}>
-            
-          </Route>
-        </Routes>
-      </div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/post" element={<PostDetails />} />
+      </Routes>
     </BrowserRouter>
   );
 };
---
+++ src/pages/PostDetails.jsx
@@ -0,0 +1,7 @@
+export default function PostDetails() {
+  return (
+    <div>
+      Post Details
+    </div>
+  );
+}
